41afc1a926530fc477d52673b889e30507e4361c
Xin Shan <xin.shan_88@outlook.com>
2019-11-09 14:45:45 -0800

    Add user input to the console program

--- Stock.py
+++ Stock.py
@@ -6,11 +6,16 @@ import matplotlib.pyplot as plt
 key = 'UKBXLMP0KH34XX09'
 ts = TimeSeries(key, output_format='pandas')
 ti = TechIndicators(key)
-aapl_data, aapl_meta_data = ts.get_daily(symbol='AAPL')
-aapl_sma, aapl_meta_sma = ti.get_sma(symbol='AAPL')
+print("Stock Historic graph.")
+userSymbol = ''
+while (userSymbol != 'quit'):
+    userSymbol = str(input("Type in the stock code for your desired stock historical quotes: ")).upper()
+    stock_data, stock_meta_data = ts.get_daily(symbol=userSymbol)
+    stock_sma, stock_meta_sma = ti.get_sma(symbol=userSymbol)
+    figure(num=None, figsize=(15,6), dpi=80, facecolor='w', edgecolor='k')
+    stock_data['4. close'].plot()
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
 
-figure(num=None, figsize=(15,6), dpi=80, facecolor='w', edgecolor='k')
-aapl_data['4. close'].plot()
-plt.tight_layout()
-plt.grid()
-plt.show()
+
